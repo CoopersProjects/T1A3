bc802b074cf7cdc5e525c1eceaeb45bbf8344a79
CoopersProjects <cooperscott01@outlook.com>
2023-12-21 21:47:58 +1100

    added add entry method

--- src_code/main.py
+++ src_code/main.py
@@ -7,4 +7,13 @@ class WildlifeConservation:
     def __init__(self):
         self.entries = {}
         self.filename = 'entries.json'  #file name to store animal info
+        
+def add_entry(self, name, species, age, zone, date_rescued):
+        if name not in self.entries:
+            current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.entries[name] = {'species': species, 'age': age, 'zone': zone, 'date_rescued': date_rescued, 'added_at': current_date}
+            print(f'{Fore.GREEN}{name} has been added to the conservation.{Style.RESET_ALL}')
+            self.save_entries()
+        else:
+            print(f'{Fore.RED}{name} is already in the conservation. Use update_entry() to update information.{Style.RESET_ALL}')
 
