1a35490a4cfee762a3a693c592a2e193df98b194
CoopersProjects <cooperscott01@outlook.com>
2023-12-21 21:35:23 +1100

    added imports that I will use

--- src_code/main.py
+++ src_code/main.py
@@ -1,4 +1,10 @@
+import json
+import os
+from colorama import Fore, Style
+from datetime import datetime
+
 class WildlifeConservation:
     def __init__(self):
         self.entries = {}
         self.filename = 'entries.json'  #file name to store animal info
+
